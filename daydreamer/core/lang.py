88725991562e17c9255c3ba173c5768e686683ad
Mike Kibbel <mkibbel@gmail.com>
2014-01-02 21:26:06 -0500

    Added simple language utilities for dictionaries and lists.

---
+++ daydreamer/core/lang.py
@@ -0,0 +1,45 @@
+from __future__ import unicode_literals
+
+import copy as copying
+
+from django.utils import six
+
+
+__all__ = ("extended", "updated",)
+
+
+def extended(destination, source, copy=False):
+    """
+    Extends the destination list with the given source iterable. Optionally
+    makes a shallow copy of the destination before the update. Returns the
+    extended list.
+    
+    """
+    if copy:
+        destination = copying.copy(destination)
+    if callable(getattr(destination, 'extend', None)):
+        destination.extend(source)
+    else:
+        for value in source:
+            destination.append(value)
+    return destination
+
+
+def updated(destination, source, copy=False):
+    """
+    Updates the destination dictionary or dictionary-like object with the
+    source dictionary. Optionally makes a shallow copy of the destination
+    before the update. Returns the updated dictionary.
+    
+    """
+    if copy:
+        if callable(getattr(destination, 'copy', None)):
+            destination = destination.copy()
+        else:
+            destination = copying.copy(destination)
+    if callable(getattr(destination, 'update', None)):
+        destination.update(source)
+    else:
+        for key, value in six.iteritems(source):
+            destination[key] = value
+    return destination
