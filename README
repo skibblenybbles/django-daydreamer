0c074a3794cbbc04835379eabc386333d7d40ff0
Mike Kibbel <mkibbel@gmail.com>
2014-01-02 19:13:51 -0500

    Set up daydreamer Django app and an example project.

---
+++ README
@@ -0,0 +1,1 @@
+A Django utility library
---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+import daydreamer
+
+
+def read(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as source:
+        return source.read()
+
+
+setup(
+    name="django-daydreamer",
+    version=daydreamer.__version__,
+    description="A Django utility library",
+    long_description=read("README"),
+    author="Mike Kibbel",
+    author_email="mkibbel@gmail.com",
+    url="https://github.com/skibblenybbles/django-daydreamer",
+    license="MIT License",
+    platforms=["OS Independent"],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
+    packages=find_packages(exclude=["example", "example.*"]),
+    install_requires=[
+        "Django>=1.6",
+    ],
+)
